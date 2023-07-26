0058b07ce72fdfea3fca6980041a9fa63081305e
akpanphilip <akpanphilip1122@gmail.com>
2023-07-26 13:46:46 +0100

    added splash screen

---
+++ lib/src/constants/image_strings.dart
@@ -0,0 +1,1 @@
+const String logoImage = "assets/images/logo.png";
---
+++ lib/src/features/authentication/controllers/splash_screen_controller.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter_firebase_auth/src/features/authentication/screens/splash_screen/welcome_screen.dart';
+import 'package:get/get.dart';
+
+class SplashScreenController extends GetxController {
+  static SplashScreenController get find => Get.find();
+
+  RxBool animate = false.obs;
+
+  Future startAnimation() async {
+    await Future.delayed(Duration(milliseconds: 500));
+    animate.value = true;
+    await Future.delayed(Duration(milliseconds: 5000));
+    Get.to(WelcomeScreen());
+    // Navigator.pushReplacement(
+    //     context, MaterialPageRoute(builder: (context) => WelcomeScreen()));
+  }
+}
