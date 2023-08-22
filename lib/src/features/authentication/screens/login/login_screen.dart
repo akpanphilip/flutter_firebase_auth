ca05f4d60b5f14a191942ae4253a3f7124c71025
akpanphilip <akpanphilip1122@gmail.com>
2023-08-22 11:57:45 +0100

    added showModalButtomSheet on login screen

--- lib/src/features/authentication/screens/login/login_screen.dart
+++ lib/src/features/authentication/screens/login/login_screen.dart
@@ -64,7 +64,53 @@ class _LoginScreenState extends State<LoginScreen> {
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
                             TextButton(
-                              onPressed: () {},
+                              onPressed: () {
+                                showModalBottomSheet(
+                                    context: context,
+                                    builder: (context) => Container(
+                                          padding: EdgeInsets.all(30),
+                                          child: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              Text(
+                                                'Password Reset!',
+                                                style: GoogleFonts.poppins(
+                                                    color: tDarkColor,
+                                                    fontSize: 20,
+                                                    fontWeight:
+                                                        FontWeight.w700),
+                                              ),
+                                              Text(
+                                                'Select one of the options given below to reset your password.',
+                                                style: GoogleFonts.poppins(
+                                                    color: tDarkColor,
+                                                    fontSize: 15,
+                                                    fontWeight:
+                                                        FontWeight.w400),
+                                              ),
+                                              SizedBox(height: 30),
+                                              ResetOpt(
+                                                icon:
+                                                    Icons.mail_outline_rounded,
+                                                title: 'E-Mail',
+                                                subtitle:
+                                                    'Reset via Mail Verification',
+                                                onTap: () {},
+                                              ),
+                                              SizedBox(height: 10),
+                                              ResetOpt(
+                                                icon: Icons
+                                                    .mobile_friendly_rounded,
+                                                title: 'OTP',
+                                                subtitle:
+                                                    'Reset via Phone Verification',
+                                                onTap: () {},
+                                              ),
+                                            ],
+                                          ),
+                                        ));
+                              },
                               child: Text(
                                 'Forgot Password?',
                                 style: GoogleFonts.poppins(color: tDarkColor),
@@ -125,6 +171,55 @@ class _LoginScreenState extends State<LoginScreen> {
               ],
             ),
           ),
+        ),
+      ),
+    );
+  }
+}
+
+
+class ResetOpt extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  final String subtitle;
+  final VoidCallback onTap;
+  const ResetOpt({
+    super.key,
+    required this.icon,
+    required this.title,
+    required this.subtitle,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.grey.shade200),
+        child: Row(
+          children: [
+            Icon(icon, size: 50),
+            SizedBox(width: 10),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(title,
+                    style: GoogleFonts.poppins(
+                        color: tDarkColor,
+                        fontSize: 15,
+                        fontWeight: FontWeight.w700)),
+                Text(subtitle,
+                    style: GoogleFonts.poppins(
+                        color: tDarkColor,
+                        fontSize: 15,
+                        fontWeight: FontWeight.w400))
+              ],
+            )
+          ],
         ),
       ),
     );
