d3075411a31c2dffa3b664d1c9f7b8674ca9708d
akpanphilip <akpanphilip1122@gmail.com>
2023-08-29 16:42:53 +0100

    initialized firebase on main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_auth/firebase_options.dart';
 import 'package:flutter_firebase_auth/src/features/authentication/screens/splash_screen/splash_screen.dart';
 import 'package:get/get.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
