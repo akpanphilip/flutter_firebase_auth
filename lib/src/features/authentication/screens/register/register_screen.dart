361ea62ab47f11e28d29c25f19f0cae4edcb3969
akpanphilip <akpanphilip1122@gmail.com>
2023-08-31 20:17:32 +0100

    added signup functionality

--- lib/src/features/authentication/screens/register/register_screen.dart
+++ lib/src/features/authentication/screens/register/register_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_firebase_auth/src/constants/colors.dart';
 import 'package:flutter_firebase_auth/src/constants/image_strings.dart';
 import 'package:flutter_firebase_auth/src/constants/sizes.dart';
+import 'package:flutter_firebase_auth/src/features/authentication/controllers/signup_controller.dart';
 import 'package:flutter_firebase_auth/src/features/authentication/screens/login/login_screen.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -18,6 +19,8 @@ class RegisterScreen extends StatefulWidget {
 class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(SignUpController()); //to access the controller
+    final _formKey = GlobalKey<FormState>();
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
@@ -36,98 +39,113 @@ class _RegisterScreenState extends State<RegisterScreen> {
                 ),
                 SizedBox(height: 10),
                 Form(
+                    key: _formKey,
                     child: Container(
-                  padding: EdgeInsets.symmetric(vertical: 20.0),
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        TextFormField(
-                          decoration: InputDecoration(
-                              prefixIcon: Icon(Icons.person_outline_outlined),
-                              labelText: 'Full Name',
-                              hintText: 'Full Name',
-                              border: OutlineInputBorder()),
-                        ),
-                        SizedBox(height: 10),
-                        TextFormField(
-                          decoration: InputDecoration(
-                              prefixIcon: Icon(Icons.mail_outline),
-                              labelText: 'E-Mail',
-                              hintText: 'E-Mail',
-                              border: OutlineInputBorder()),
-                        ),
-                        SizedBox(height: 10),
-                        TextFormField(
-                          decoration: InputDecoration(
-                              prefixIcon: Icon(Icons.phone),
-                              labelText: 'Phone No',
-                              hintText: 'Phone No',
-                              border: OutlineInputBorder()),
-                        ),
-                        SizedBox(height: 10),
-                        TextFormField(
-                          decoration: InputDecoration(
-                              prefixIcon: Icon(Icons.fingerprint_rounded),
-                              suffixIcon: IconButton(
-                                  onPressed: () {},
-                                  icon: Icon(Icons.remove_red_eye_rounded)),
-                              labelText: 'Password',
-                              hintText: 'Password',
-                              border: OutlineInputBorder()),
-                        ),
-                        SizedBox(height: 10),
-                        SizedBox(
-                          width: double.infinity,
-                          child: ElevatedButton(
-                              style: OutlinedButton.styleFrom(
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10)),
-                                foregroundColor: tWhiteColor,
-                                backgroundColor: tSecondaryColor,
-                                side: BorderSide(color: tSecondaryColor),
-                                padding: EdgeInsets.symmetric(vertical: 20),
-                              ),
-                              onPressed: () {},
-                              child: Text('SIGNUP')),
-                        ),
-                        SizedBox(height: 20),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
+                      padding: EdgeInsets.symmetric(vertical: 20.0),
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text('OR'),
-                            SizedBox(height: 20),
-                            Container(
-                                height: 60,
-                                width: double.infinity,
-                                child: OutlinedButton.icon(
+                            TextFormField(
+                              controller: controller.fullName,
+                              decoration: InputDecoration(
+                                  prefixIcon:
+                                      Icon(Icons.person_outline_outlined),
+                                  labelText: 'Full Name',
+                                  hintText: 'Full Name',
+                                  border: OutlineInputBorder()),
+                            ),
+                            SizedBox(height: 10),
+                            TextFormField(
+                              controller: controller.email,
+                              decoration: InputDecoration(
+                                  prefixIcon: Icon(Icons.mail_outline),
+                                  labelText: 'E-Mail',
+                                  hintText: 'E-Mail',
+                                  border: OutlineInputBorder()),
+                            ),
+                            SizedBox(height: 10),
+                            TextFormField(
+                              controller: controller.phoneNo,
+                              decoration: InputDecoration(
+                                  prefixIcon: Icon(Icons.phone),
+                                  labelText: 'Phone No',
+                                  hintText: 'Phone No',
+                                  border: OutlineInputBorder()),
+                            ),
+                            SizedBox(height: 10),
+                            TextFormField(
+                              controller: controller.password,
+                              decoration: InputDecoration(
+                                  prefixIcon: Icon(Icons.fingerprint_rounded),
+                                  suffixIcon: IconButton(
+                                      onPressed: () {},
+                                      icon: Icon(Icons.remove_red_eye_rounded)),
+                                  labelText: 'Password',
+                                  hintText: 'Password',
+                                  border: OutlineInputBorder()),
+                            ),
+                            SizedBox(height: 10),
+                            SizedBox(
+                              width: double.infinity,
+                              child: ElevatedButton(
                                   style: OutlinedButton.styleFrom(
                                     shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(10.0),
-                                    ),
+                                        borderRadius:
+                                            BorderRadius.circular(10)),
+                                    foregroundColor: tWhiteColor,
+                                    backgroundColor: tSecondaryColor,
+                                    side: BorderSide(color: tSecondaryColor),
+                                    padding: EdgeInsets.symmetric(vertical: 20),
                                   ),
-                                  onPressed: () {},
-                                  icon: Image(
-                                      image: AssetImage(google),
-                                      width: 30,
-                                      height: 30),
-                                  label:
-                                      Text('Sign in with Google'.toUpperCase()),
-                                ))
-                          ],
-                        ),
-                        SizedBox(height: 10),
-                        Align(
-                          alignment: Alignment.center,
-                          child: TextButton(
-                            onPressed: () => Get.to(() => const LoginScreen()),
-                            child: Text(
-                              'Already have an Account?'.toUpperCase(),
-                              style: GoogleFonts.poppins(color: tDarkColor),
+                                  onPressed: () {
+                                    if (_formKey.currentState!.validate()) {
+                                      SignUpController.instance.registerUser(
+                                          controller.email.text.trim(),
+                                          controller.password.text.trim());
+                                    }
+                                  },
+                                  child: Text('SIGNUP')),
                             ),
-                          ),
-                        ),
-                      ]),
-                ))
+                            SizedBox(height: 20),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text('OR'),
+                                SizedBox(height: 20),
+                                Container(
+                                    height: 60,
+                                    width: double.infinity,
+                                    child: OutlinedButton.icon(
+                                      style: OutlinedButton.styleFrom(
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(10.0),
+                                        ),
+                                      ),
+                                      onPressed: () {},
+                                      icon: Image(
+                                          image: AssetImage(google),
+                                          width: 30,
+                                          height: 30),
+                                      label: Text(
+                                          'Sign in with Google'.toUpperCase()),
+                                    ))
+                              ],
+                            ),
+                            SizedBox(height: 10),
+                            Align(
+                              alignment: Alignment.center,
+                              child: TextButton(
+                                onPressed: () =>
+                                    Get.to(() => const LoginScreen()),
+                                child: Text(
+                                  'Already have an Account?'.toUpperCase(),
+                                  style: GoogleFonts.poppins(color: tDarkColor),
+                                ),
+                              ),
+                            ),
+                          ]),
+                    ))
               ],
             ),
           ),
